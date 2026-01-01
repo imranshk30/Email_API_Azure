a753ed150ccaee5f46b9fc3132506af81238c05f
imran sheikh <imranshk30@gmail.com>
2026-01-01 12:45:34 -0500

    2000

--- InboxEngine.Api/Controllers/InboxController.cs
+++ InboxEngine.Api/Controllers/InboxController.cs
@@ -49,7 +49,7 @@ public class InboxController : ControllerBase
         _logger.LogInformation("Returning {Count} sorted emails", emails.Count);
         return Ok(scoredemails);
 
-       //featured part 2
+       //featured part 2000
        // throw new NotImplementedException("Sort endpoint not yet implemented");
     }
 }
