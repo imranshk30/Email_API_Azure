fdd21080ef813e2ed9b1813b02b4eef4d8e317b0
imranshk30 <imranshk30@gmail.com>
2025-12-31 20:22:04 -0500

    Update InboxController.cs

--- InboxEngine.Api/Controllers/InboxController.cs
+++ InboxEngine.Api/Controllers/InboxController.cs
@@ -48,7 +48,7 @@ public class InboxController : ControllerBase
         }).OrderByDescending(e => e.PriorityScore).ToList();
         _logger.LogInformation("Returning {Count} sorted emails", emails.Count);
         return Ok(scoredemails);
-        //test 6
+        //test 7
        // throw new NotImplementedException("Sort endpoint not yet implemented");
     }
 }
